bc0e7bbeb3a09bb0c1701f7f37bb16c493d22e8c
Loïc Turpin <loic@MBP-Loic.local>
2017-11-09 19:02:24 +0100

    fix unreferenced handlers

--- index.js
+++ index.js
@@ -25,50 +25,55 @@ arduinoUSB1.on('READY', ()=>{
 	arduinoUSB1.writeSerial('HELLO', console.log);
 });
 
-let startTourelle = (handler)=> {
-	arduinoTourelle = handler;
+let initTourelle = (arduinoHandler)=> {
+	arduinoTourelle = arduinoHandler;
 	arduinoTourelleReady = true;
+
 	arduinoTourelle.writeSerial('START', console.log);
+
+	arduinoTourelle.on('MEAS', (data)=>{
+		if(currentSocket != undefined) {
+			
+			let msg = {};
+			msg.angle = data.args[0];
+			msg.distA = data.args[1];
+			msg.distB = data.args[2];
+			msg.distC = data.args[3];
+			msg.distD = data.args[4];
+			currentSocket.emit('meas', msg);
+		}
+	});
 }
-let startPropulsion = (handler)=> {
-	arduinoPropulsion = handler;
+let initPropulsion = (arduinoHandler)=> {
+	arduinoPropulsion = arduinoHandler;
 	arduinoPropulsionReady = true;
 
+	arduinoPropulsion.on('FINISHED', (data)=>{
+		console.log('MOVE FINISHED', data.args);
+	});
+
 	// TODO
+
+	arduinoPropulsion.writeSerial('MOVE:1:6000:0.09:15:80');
 }
 
 arduinoUSB0.on('TOURELLE', ()=>{
 	console.log('Arduino TOURELLE is on USB0');
-	startTourelle(arduinoUSB0);
+	initTourelle(arduinoUSB0);
 });
 arduinoUSB0.on('PROPULSION', ()=>{
 	console.log('Arduino PROPULSION is on USB0');
-	startPropulsion(arduinoUSB0);
+	initPropulsion(arduinoUSB0);
 });
 arduinoUSB1.on('TOURELLE', ()=>{
 	console.log('Arduino TOURELLE is on USB1');
-	startTourelle(arduinoUSB1);
+	initTourelle(arduinoUSB1);
 });
 arduinoUSB1.on('PROPULSION', ()=>{
 	console.log('Arduino PROPULSION is on USB1');
-	startPropulsion(arduinoUSB1);
+	initPropulsion(arduinoUSB1);
 });
 
-arduinoTourelle.on('MEAS', (data)=>{
-	if(currentSocket != undefined) {
-		let msg = {};
-		msg.angle = data.args[0];
-		msg.distA = data.args[1];
-		msg.distB = data.args[2];
-		msg.distC = data.args[3];
-		msg.distD = data.args[4];
-		currentSocket.emit('meas', msg);
-	}
-});
-
-arduinoPropulsion.on('FINISHED', (data)=>{
-	console.log('MOVE FINISHED', data.args);
-});
 
 app.use('/', express.static(path.join(__dirname, 'html_root')));
 
