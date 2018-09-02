e7c1749375b42b58cbd0b5515a90a3318604ae94
Loïc Turpin <loic@MBP-Loic.local>
2018-09-02 19:19:30 +0200

    test stepper js

--- testStepperMotor.js
+++ testStepperMotor.js
@@ -8,6 +8,6 @@ const gpioPinNumberPulse = 17;
 const gpioPinNumberDirA = 27;
 const gpioPinNumberDirB = 22;
 
-const steppers = new StepperMotors(gpioPinNumberPulse, gpioPinNumberDirA, gpioPinNumberDirB, done, 3200);
+const steppers = new StepperMotors(gpioPinNumberPulse, gpioPinNumberDirA, gpioPinNumberDirB, done, 200);
 
-steppers.forward(16000);
+steppers.forward(900);
